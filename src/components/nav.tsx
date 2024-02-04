361653387d0048fcc70f62d4387a79dc2399d1be
appxpy <appxpy@appxpy.com>
2024-02-04 12:58:57 +0300

    init

---
+++ src/components/nav.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactComponent as LogoSVG } from "../assets/ae-logo.svg";
+
+interface LogoProps {
+  size: number;
+}
+
+export const Logo: React.FC<LogoProps> = ({ ...props }: LogoProps) => {
+  const hw = `${props.size}px`;
+  return (
+    <div
+      className="relative flex items-center justify-center group"
+      style={{ width: hw, height: hw }}
+    >
+      <LogoSVG
+        width={hw}
+        height={hw}
+        className="stroke-primary stroke-[5px] p-1 hover:fill-transparent hover:opacity-70 fill-primary transition-all duration-300"
+      ></LogoSVG>
+    </div>
+  );
+};
